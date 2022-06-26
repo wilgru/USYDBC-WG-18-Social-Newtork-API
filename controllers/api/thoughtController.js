aeeb22d95a46cafe18708e87a2ce9313f9b1c52c
wilgru <william.gruszka@gmail.com>
2022-06-27 09:57:40 +1000

    added routes for adding and deleting reactions

--- controllers/api/thoughtController.js
+++ controllers/api/thoughtController.js
@@ -4,20 +4,22 @@ const { User, Thought } = require('../../models');
 // get all thoughts
 router.get('/', async (req, res) => {
     try{
-        const thoughts = await Thought.find();
+        const thoughts = await Thought.find().populate('reactions');
         res.status(200).json(thoughts);
     } catch (e) {
-        console.log(e);
+        console.log(e)
+        res.status(400).send('400 - error');
     }
 })
 
 // get a thought by id
-router.get('/:thoughtid', async (req, res) => {
+router.get('/:thoughtId', async (req, res) => {
     try{
-        const thought = await User.where('_id').equals(req.params.thoughtid);
+        const thought = await User.where('_id').equals(req.params.thoughtId);
         res.status(200).json(thought);
     } catch (e) {
-        console.log(e);
+        console.log(e)
+        res.status(400).send('400 - error');
     }
 });
 
@@ -27,64 +29,71 @@ router.post('/', async (req, res) => {
         const newThought = await Thought.create({ thoughtText: req.body.thoughtText, username: req.body.username });
         const usersThought = await User.where('_id').equals(req.body.userId);
         
-        usersThought.thoughts.push(newThought._id);
-        usersThought.save();
+        console.log(usersThought[0])
+        usersThought[0].thoughts.push(newThought._id);
+        usersThought[0].save();
 
         res.status(201).json(newThought);
     } catch (e) {
-        console.log(e);
+        console.log(e)
+        res.status(400).send('400 - error');
     }
 });
 
 // update thought
-router.put('/:userid', async (req, res) => {
+router.put('/:thoughtId', async (req, res) => {
     try{
-        const thought = await Thought.where('_id').equals(req.params.thoughtid);
-        if (req.body.username) {thought[0].username = req.body.username}
+        const thought = await Thought.where('_id').equals(req.params.thoughtId);
         if (req.body.thoughtText) {thought[0].thoughtText = req.body.thoughtText}
-        await thought.save();
+        if (req.body.username) {thought[0].username = req.body.username}
+        await thought[0].save();
         res.status(201).json(thought);
     } catch (e) {
-        console.log(e);
+        console.log(e)
+        res.status(400).send('400 - error');
     }
 });
 
 // delete thought
-router.delete('/:thoughtid', async (req, res) => {
+router.delete('/:thoughtId', async (req, res) => {
     try{
-        const deletedThought = await Thought.deleteOne({ _id: req.params.thoughtid })
+        const deletedThought = await Thought.deleteOne({ _id: req.params.thoughtId })
         res.status(200).json(deletedThought);
     } catch (e) {
-        console.log(e);
+        console.log(e)
+        res.status(400).send('400 - error');
     }
 });
 
 // add new reaction
-router.post('/:thoughtid/reactions', (req, res) => {
+router.post('/:thoughtId/reactions', async (req, res) => {
     try {
-        const thought = Thought.where('_id').equals(req.params.thoughtid);
-        thought[0].reactions.push(req.body);
-        thought.save();
+        const thought = await Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $push: { reactions: req.body } },
+            { new: true }
+        );
 
         res.status(201).json(thought);
     } catch (e) {
-        console.log(e);
+        console.log(e)
+        res.status(400).send('400 - error');
     }
 });
 
 // delete reaction
-router.delete('/:thoughtid/reactions/:reactionId', (req, res) => {
+router.delete('/:thoughtId/reactions/:reactionId', async (req, res) => {
     try {
-        const thought = Thought.where('_id').equals(req.params.thoughtid);
-        thought.findOneAndUpdate(
-            { _id: req.params.thoughtid },
-            { $pull: {reactions: req.body.reactionId} },
+        const thought = await Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $pull: { reactions: { _id: req.params.reactionId } } },
             { runValidators: true, new: true }
         );
 
         res.status(200).json(thought);
     } catch (e) {
-        console.log(e);
+        console.log(e)
+        res.status(400).send('400 - error');
     }
 })
 
