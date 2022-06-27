06e8ee0f0d45d0fda4d6e9454795e71c968cb256
wilgru <william.gruszka@gmail.com>
2022-06-27 12:02:19 +1000

    updated mongoose connection function

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,4 @@
-const { connect, connection } = require('mongoose');
+const { connect } = require('mongoose');
 
 const connectionString =
   process.env.MONGODB_URI || 'mongodb://localhost:27017/socialNetworkDB';
