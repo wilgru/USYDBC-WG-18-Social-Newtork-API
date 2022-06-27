8eb9ce58a2c00a32fc6ccd6b7e82cb9884407f22
wilgru <william.gruszka@gmail.com>
2022-06-27 14:40:20 +1000

    renamed routers

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./userController');
 const thoughtRoutes = require('./thoughtController');
 
-router.use('/user', userRoutes);
-router.use('/thought', thoughtRoutes);
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
