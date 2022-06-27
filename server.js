cf5c54baccd6d3af59efd3cbd461cd82f2bfc454
wilgru <william.gruszka@gmail.com>
2022-06-27 14:19:22 +1000

    removed unused code

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const db = require('./config/connection');
 const routes = require('./controllers');
 
-const cwd = process.cwd();
-
 const PORT = process.env.PORT || 3001;
 const app = express();
 
